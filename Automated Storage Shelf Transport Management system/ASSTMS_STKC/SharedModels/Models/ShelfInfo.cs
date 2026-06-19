f4ebbdcad9101feafb1ea3c1fbe2a32485031cee
yuyanagano-youth <ynagano@youtheng.co.jp>
2026-06-19 17:51:08 +0900

    models追加

--- Automated Storage Shelf Transport Management system/ASSTMS_STKC/SharedModels/Models/ShelfInfo.cs
+++ Automated Storage Shelf Transport Management system/ASSTMS_STKC/SharedModels/Models/ShelfInfo.cs
@@ -2,5 +2,9 @@
 {
     public class ShelfInfo
     {
+        public string StokerId { get; set; }
+        public string ShelfName { get; set; }
+        public string CarrierId { get; set; }
+        public DateTime InTime { get; set; }
     }
 }
--- Automated Storage Shelf Transport Management system/ASSTMS_STKC/SharedModels/Models/StockInfo.cs
+++ Automated Storage Shelf Transport Management system/ASSTMS_STKC/SharedModels/Models/StockInfo.cs
@@ -2,5 +2,10 @@
 {
     public class StockInfo
     {
+        public string StockerName { get; set; }
+        public string Status { get; set; }
+        public string ConnectionStatus { get; set; }
+        public string OperationState { get; set; }
+        public DateTime LastHeartbeat { get; set; }
     }
 }
